44405342be8e86a0e28c715c2e4c6634905b04f8
japenb <124191170+japenb@users.noreply.github.com>
2023-03-28 15:45:43 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+// const button = document.querySelector('#button')
+
+// const toogleMenu = (e) => {
+//     console.log('hello!')
+//     const menu = document.querySelector('#menu-mobile')
+//     menu.style.display == 'none' ? menu.style.display = 'block': menu.style.display = 'none'
+//     e.preventDefault()
+// }
+
+// button.addEventListener('click', toogleMenu)
